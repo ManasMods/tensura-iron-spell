e362bb1708bc068964d289644aac60ba65b968b0
Minh <minh1604.work@gmail.com>
2026-04-09 01:56:59 +0700

    Iron Spell set up

---
+++ common/src/main/java/io/github/manasmods/tensura_iron/TensuraIron.java
@@ -0,0 +1,8 @@
+package io.github.manasmods.tensura_iron;
+
+public final class TensuraIron {
+    public static final String MOD_ID = "tensura_iron_spells";
+
+    public static void init() {
+    }
+}
---
+++ neoforge/src/main/java/io/github/manasmods/tensura_iron/neoforge/TensuraIronNeoForge.java
@@ -0,0 +1,12 @@
+package io.github.manasmods.tensura_iron.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import io.github.manasmods.tensura_iron.TensuraIron;
+
+@Mod(TensuraIron.MOD_ID)
+public final class TensuraIronNeoForge {
+    public TensuraIronNeoForge() {
+        TensuraIron.init();
+    }
+}
